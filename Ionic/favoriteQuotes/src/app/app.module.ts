ae6ed186813158efcd5aff99e80025a9551a4a63
Pablo Mezzon Kintopp <pablokintopp@gmail.com>
2018-04-25 22:15:32 -0300

    Ionic Quotes app created + added empty pages

---
+++ Ionic/favoriteQuotes/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { TabsPage } from './../pages/tabs/tabs';
+import { LibraryPage } from './../pages/library/library';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { FavoritesPage } from '../pages/favorites/favorites';
+import { SettingsPage } from '../pages/settings/settings';
+import { QuotePage } from '../pages/quote/quote';
+import { QuotesPage } from '../pages/quotes/quotes';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TabsPage,
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage
+    
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TabsPage,
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage
+    
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ Ionic/favoriteQuotes/src/pages/tabs/tabs.ts
@@ -0,0 +1,20 @@
+import { LibraryPage } from './../library/library';
+import { Component } from "@angular/core";
+import { FavoritesPage } from "../favorites/favorites";
+
+@Component({
+    selector: 'page-tabs',
+    template:`
+    <ion-tabs>
+        <ion-tab [root]="favoritesPage" tabTitle="Favorites" tabIcon="star" > </ion-tab>
+        <ion-tab [root]="libraryPage" tabTitle="Library" tabIcon="book" ></ion-tab>
+    </ion-tabs>
+    `
+    
+    
+})
+export class TabsPage{
+    favoritesPage  = FavoritesPage;
+    libraryPage = LibraryPage;
+
+}
